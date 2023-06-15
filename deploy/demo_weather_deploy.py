d13b0440135c9b8f98c7b3028fa92410f190f3f9
m-tok <meneksetok1907@gmail.com>
2023-06-15 13:41:39 -0700

    function name changed in demo_weather_deploy.py

--- deploy/demo_weather_deploy.py
+++ deploy/demo_weather_deploy.py
@@ -1,8 +1,8 @@
-from demo_weather import my_flow
+from demo_weather import fetch_weather
 from prefect.deployments import Deployment
 
 deployment = Deployment.build_from_flow(
-    flow=my_flow, 
+    flow=fetch_weather, 
     name="example_from_deployment", 
     work_queue_name="dev",
     version="1", 
