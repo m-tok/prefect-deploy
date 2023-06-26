69ad7707cb402579bbce0df5739c225adb32ec4b
m-tok <meneksetok1907@gmail.com>
2023-06-26 10:46:18 -0700

    k8s

--- blocks/k8s.py
+++ blocks/k8s.py
@@ -4,6 +4,6 @@ k8s_job = KubernetesJob(
     namespace="prefect",
     image="prefecthq/prefect:2-python3.9",
     image_pull_policy=KubernetesImagePullPolicy.IF_NOT_PRESENT,
-) 
+)
 
-k8s_job.save("test-k8s-job",overwrite=True)
+k8s_job.save("test-k8s-job",overwrite=True )
