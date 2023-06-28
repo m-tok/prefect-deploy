fc580e03c03ceb25808bb0297734cfdeab9bd9c1
m-tok <meneksetok1907@gmail.com>
2023-06-28 07:56:46 -0700

    dog_deploy.py updated

--- deploy/dog_example/dog_deploy.py
+++ deploy/dog_example/dog_deploy.py
@@ -1,3 +1,11 @@
+from prefect.filesystems import GitHub
+
+github_block = GitHub.load("githubk8s")
+
+from prefect.infrastructure.kubernetes import KubernetesJob
+
+kubernetes_job_block = KubernetesJob.load("k8s")
+
 from dog_flow import dog
 from prefect.deployments import Deployment
 
@@ -5,6 +13,9 @@ deployment = Deployment.build_from_flow(
     flow=dog,
     name= "dog-deploy-from-python",
     parameters= {"num_barks": 7},
+    storage= github_block,
+    infrastructure= kubernetes_job_block
+
 )
 
 deployment.apply()
