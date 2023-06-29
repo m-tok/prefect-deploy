63988e63fcadffcf9bc1aa7148c0078eed13d5d5
m-tok <meneksetok1907@gmail.com>
2023-06-29 09:59:39 -0700

    dog_flow.py updated

--- flows/dog_flow.py
+++ flows/dog_flow.py
@@ -1,6 +1,6 @@
 from prefect.infrastructure.kubernetes import KubernetesJob
 
-kubernetes_job_block = KubernetesJob.load("test-k8s-job")
+kubernetes_job_block = KubernetesJob.load("k8s")
 from prefect import flow, task, get_run_logger
 
 @task
