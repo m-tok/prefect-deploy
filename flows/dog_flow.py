99c76da1e2236954d25d51a00d47b78b0846dcd7
m-tok <meneksetok1907@gmail.com>
2023-06-29 10:49:26 -0700

    Revert "dog_flow.py updated"
    This reverts commit 63988e63fcadffcf9bc1aa7148c0078eed13d5d5.

--- flows/dog_flow.py
+++ flows/dog_flow.py
@@ -1,6 +1,6 @@
 from prefect.infrastructure.kubernetes import KubernetesJob
 
-kubernetes_job_block = KubernetesJob.load("k8s")
+kubernetes_job_block = KubernetesJob.load("test-k8s-job")
 from prefect import flow, task, get_run_logger
 
 @task
