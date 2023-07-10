927de8e40306b20d5e92b603226d4263e18fe59f
m-tok <meneksetok1907@gmail.com>
2023-07-10 12:22:54 -0700

    access token changed

--- blocks/github.py
+++ blocks/github.py
@@ -3,7 +3,7 @@ from prefect.filesystems import GitHub
 block = GitHub(
     repository=f"https://github.com/m-tok/prefect-deploy.git",
     reference="main",
-    access_token="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",
+    access_token="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",
 )
 
 block_name = "my-git-repo"
